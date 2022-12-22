09235dc34a17aeb8629bd9961d59ff21c59474d0
JP <85654561+jp524@users.noreply.github.com>
2022-12-22 18:32:21 -0500

    Modify User#show view to have profile info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @profile = @user.profile
     @posts = @user.posts
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,9 +4,13 @@ class Profile < ApplicationRecord
   validates :name, presence: true
   validates :location, presence: true
   validates :birthday, presence: true
-  validate :user_has_to_be_at_least_13
+  validate :user_has_to_be_at_least_thirteen
 
-  def user_has_to_be_at_least_13
+  def user_has_to_be_at_least_thirteen
     errors.add(:birthday, 'is not acceptable. User must be at least 13 years old') if birthday > Date.today - 13.years
+  end
+
+  def formatted_birthday
+    birthday.strftime('%B %d')
   end
 end
