9749d6d654dfedb6140dcd460d7bc47a8d090031
JP <85654561+jp524@users.noreply.github.com>
2022-12-22 18:20:12 -0500

    Modify seeds to include Profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: false
 
 Post.delete_all
+Profile.delete_all
 User.delete_all
 
 10.times do
+  name = Faker::Name.name
+
   user = User.create(
-    email: Faker::Internet.email,
+    email: Faker::Internet.email(name: name),
     password: Faker::Internet.password
+  )
+
+  Profile.create(
+    name: name,
+    location: "#{Faker::Address.city}, #{Faker::Address.country}",
+    birthday: Faker::Date.birthday(min_age: 12, max_age: 90),
+    user_id: user.id
   )
 
   3.times do
@@ -18,6 +28,13 @@ User.delete_all
 end
 
 known_user = User.create(email: 'test@email.com', password: 'abc123')
+
+Profile.create(
+  name: 'Jane Smith',
+  location: 'New York City, USA',
+  birthday: '1990-12-12',
+  user_id: known_user.id
+)
 
 3.times do
   Post.create(
