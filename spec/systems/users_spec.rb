0fde954c4cc2ca19e62a2d50d9b058ebb28d81aa
JP <85654561+jp524@users.noreply.github.com>
2022-12-23 09:11:48 -0500

    Add devise views for User

--- spec/systems/users_spec.rb
+++ spec/systems/users_spec.rb
@@ -18,4 +18,16 @@ RSpec.describe 'Users system', type: :system do
 
     expect(current_path).to have_content('/')
   end
+
+  it 'creates a new user' do
+    visit('/')
+    click_link('Sign up')
+
+    fill_in('Email', with: 'new_user_test@example.com')
+    fill_in('Password', with: 'abc123')
+    fill_in('Password confirmation', with: 'abc123')
+    click_button('Sign up')
+
+    expect(User.last.email).to eq('new_user_test@example.com')
+  end
 end
