170ad519e496500e5b932cdfd70413a4a1822e96
JP <85654561+jp524@users.noreply.github.com>
2023-01-21 10:20:40 -0500

    Fix bugs related to Users and Posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,12 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
-    @users.reject { |user| user == current_user }
+    @users = User.excluding(current_user)
   end
 
   def show
     @user = User.find(params[:id])
     @profile = @user.profile
-    @posts = @user.posts
+    @posts = @user.posts.sorted_by_date
   end
 end
