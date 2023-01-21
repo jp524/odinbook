ef1caa0e6b1c1145d305b36688cc1051040b8d71
JP <85654561+jp524@users.noreply.github.com>
2023-01-21 11:36:03 -0500

    Add eager loading for User, Post, Profile and Like associations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.excluding(current_user)
+    @users = User.excluding(current_user).includes(:profile)
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:profile, :posts).find(params[:id])
     @profile = @user.profile
     @posts = @user.posts.sorted_by_date
   end
