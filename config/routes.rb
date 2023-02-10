ec85fa13642a18f4f72c927a30a05444679feb5d
JP <85654561+jp524@users.noreply.github.com>
2023-02-10 06:24:05 -0500

    Add deletion of comments by author

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'posts#index'
   resources :users, only: %i[index show]
   resources :posts, only: %i[index new create] do
-    resources :comments, only: %i[new create]
+    resources :comments, only: %i[new create destroy]
   end
   resources :profiles, only: %i[edit update]
   resources :friend_requests, only: %i[create update]
