a4b83765c1cf66e3a4e512882a957e50f4d17a10
JP <85654561+jp524@users.noreply.github.com>
2023-02-22 21:20:07 -0500

    Modify Registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
 
-  def create
-    set_flash_message! :notice, :signed_up, :now
-  end
-
-  protected
-
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: permitted_attributes)
   end
